2b7296e95e34d451a9a2d7bff806888a754892c9
john Mario <john1646@hotmail.com>
2021-05-23 21:41:18 -0500

    footer & header php

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Orient Hause</title>
-    <link rel="stylesheet" href="/cesde/css/styles.css">
-</head>
-<body>
-    
-</body>
-</html>
+<?php include_once ("/opt/lampp/htdocs/cesde/static/header.php") ?>
+
+<?php include_once ("/opt/lampp/htdocs/cesde/static/footer.php") ?>
